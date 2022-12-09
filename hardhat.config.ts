e3318ad01c893724b657dcb6f75728aa086fc09f
MuneebKhan1996 <rmkhan1996@hotmail.com>
2022-12-09 17:02:26 +0500

    deployed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import "@nomiclabs/hardhat-waffle";
-import { HardhatUserConfig } from "hardhat/config";
+import '@nomiclabs/hardhat-waffle';
+import 'dotenv/config';
+import { HardhatUserConfig } from 'hardhat/config';
+
+const CELO_URL = process.env.CELO_URL as string;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
+  networks: {
+    celo: {
+      url: CELO_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
