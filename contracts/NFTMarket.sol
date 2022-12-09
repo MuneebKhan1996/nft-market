c6e0194adacc1b6899b646b49f26814e90d49c6d
MuneebKhan1996 <rmkhan1996@hotmail.com>
2022-12-09 12:52:38 +0500

    buyNFTs tests working

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -2,10 +2,30 @@
 pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTMarket is ERC721URIStorage {
+struct NFTListing {
+    uint256 price;
+    address seller;
+}
+
+contract NFTMarket is ERC721URIStorage, Ownable {
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
+    mapping(uint256 => NFTListing) private _listings;
+
+    //if tokenURI is not and empty string then an NFT was created
+    //if price is not zero then an NFT was listed
+    //if price is 0 && tokenURI is an empty string then an NFT was transferred (either bought or the listing was cancelled)
+    event NFTTransfer(
+        uint256 tokenId,
+        address to,
+        string tokenURI,
+        uint256 price
+    );
 
     constructor() ERC721("B NFTs", "BNFT") {}
 
@@ -16,13 +36,54 @@ contract NFTMarket is ERC721URIStorage {
         _safeMint(msg.sender, currentTokenId);
         _setTokenURI(currentTokenId, tokenURI);
 
+        emit NFTTransfer(currentTokenId, msg.sender, tokenURI, 0);
+
         return currentTokenId;
     }
 
     //ListNFT
-    function listNFT(uint256 tokenId, uint256 price) public {}
+    function listNFT(uint256 tokenId, uint256 price) public {
+        require(price > 0, "NFTMarket: price must be greater than zero");
+        transferFrom(msg.sender, address(this), tokenId);
+        _listings[tokenId] = NFTListing(price, msg.sender);
+
+        emit NFTTransfer(tokenId, address(this), "", price);
+    }
 
     //BuyNFT
+    function buyNFT(uint256 tokenId) public payable {
+        NFTListing memory listing = _listings[tokenId];
+        require(listing.price > 0, "NFT not listed for sale");
+        require(msg.value == listing.price, "NFT price incorrect");
+
+        ERC721(address(this)).transferFrom(address(this), msg.sender, tokenId);
+        clearListing(tokenId);
+        payable(listing.seller).transfer(listing.price.mul(95).div(100));
+
+        emit NFTTransfer(tokenId, msg.sender, "", 0);
+    }
 
     //CancelNFT
+    function cancelListing(uint256 tokenId) public {
+        NFTListing memory listing = _listings[tokenId];
+        require(listing.price > 0, "NFT not listed for sale");
+        require(listing.seller == msg.sender, "NFT: You are not the owner :/");
+
+        transferFrom(address(this), msg.sender, tokenId);
+
+        clearListing(tokenId);
+
+        emit NFTTransfer(tokenId, msg.sender, "", 0);
+    }
+
+    function withdrawFunds(address) public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "NFT balance is zero");
+        payable(owner()).transfer(balance);
+    }
+
+    function clearListing(uint256 tokenId) private {
+        _listings[tokenId].price = 0;
+        _listings[tokenId].seller = address(0);
+    }
 }
--- test/Lock.ts
+++ test/Lock.ts
@@ -1,23 +1,154 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { expect } from 'chai';
+import { Contract } from 'ethers';
 import { ethers } from 'hardhat';
 
-describe('NFTMarket', () => {
-  it('do something', async () => {
-    const NFTMarket = await ethers.getContractFactory('NFTMarket');
-    const nftMarket = await NFTMarket.deploy();
-    await nftMarket.deployed();
+describe('NFT market', () => {
+  let nftMarket: Contract;
+  let signers: SignerWithAddress[];
 
-    const tokenURI = 'https://www.token.uri';
+  const tokenURI = 'https://www.token.uri';
+
+  before(async () => {
+    const NFTMarket = await ethers.getContractFactory('NFTMarket');
+    nftMarket = await NFTMarket.deploy();
+    await nftMarket.deployed();
+    signers = await ethers.getSigners();
+  });
+
+  const createNFT = async (tokenURI: string) => {
     const transaction = await nftMarket.createNFT(tokenURI);
     const receipt = await transaction.wait();
-
     const tokenId = receipt.events[0].args.tokenId;
-    const mintedTokenURI = await nftMarket.tokenURI(tokenId);
-    expect(mintedTokenURI).to.equal(tokenURI);
+    return tokenId;
+  };
 
-    const ownerAddress = await nftMarket.ownerOf(tokenId);
-    const signers = await ethers.getSigners();
-    const currentAddress = await signers[0].getAddress();
-    expect(ownerAddress).to.equal(currentAddress);
+  const createAndListNFT = async (price: number) => {
+    const tokenId = await createNFT(tokenURI);
+    const transaction = await nftMarket.listNFT(tokenId, price);
+    await transaction.wait();
+    return tokenId;
+  };
+
+  describe('createNFT', () => {
+    it('should create an NFT with the correct owner and tokenURI', async () => {
+      const transaction = await nftMarket.createNFT(tokenURI);
+      const receipt = await transaction.wait();
+      const tokenId = receipt.events[0].args.tokenId;
+
+      const mintedTokenURI = await nftMarket.tokenURI(tokenId);
+      expect(mintedTokenURI).to.equal(tokenURI);
+
+      const ownerAddress = await nftMarket.ownerOf(tokenId);
+      const signers = await ethers.getSigners();
+      const currentAddress = await signers[0].getAddress();
+      expect(ownerAddress).to.equal(currentAddress);
+
+      //nft transfer event has correct args
+
+      const args = receipt.events[1].args;
+      expect(args.tokenId).to.equal(tokenId);
+      expect(args.to).to.equal(ownerAddress);
+      expect(args.tokenURI).to.equal(tokenURI);
+      expect(args.price).to.equal(0);
+    });
+  });
+
+  describe('ListNFT', () => {
+    it('should revert if price is 0', async () => {
+      const tokenId = await createNFT(tokenURI);
+
+      const transaction = nftMarket.listNFT(tokenId, 0);
+      await expect(transaction).to.be.revertedWith(
+        'NFTMarket: price must be greater than zero'
+      );
+    });
+
+    it('should revert if not called by the owner', async () => {
+      const tokenId = await createNFT(tokenURI);
+      const transaction = nftMarket.connect(signers[1]).listNFT(tokenId, 10);
+      await expect(transaction).to.be.revertedWith(
+        'ERC721: caller is not token owner or approved'
+      );
+    });
+
+    it('should list token for sale if all requirements are met', async () => {
+      const price = 12;
+
+      const tokenId = await createNFT(tokenURI);
+      const transaction = await nftMarket.listNFT(tokenId, price);
+      const receipt = await transaction.wait();
+
+      //Ownership should be transferred to the contract
+      const ownerAddress = await nftMarket.ownerOf(tokenId);
+      expect(ownerAddress).to.equal(nftMarket.address);
+
+      const args = receipt.events[1].args;
+
+      expect(args.tokenId).to.equal(tokenId);
+      expect(args.to).to.equal(nftMarket.address);
+      expect(args.tokenURI).to.equal('');
+      expect(args.price).to.equal(price);
+    });
+  });
+
+  describe('BuyNFT', () => {
+    it('should revert if NFT is not listed for sale', async () => {
+      const transaction = nftMarket.buyNFT(999);
+      await expect(transaction).to.be.revertedWith('NFT not listed for sale');
+    });
+
+    it('should revert if amount of wei sent is not equal to listed NFT price', async () => {
+      const tokenId = createAndListNFT(908);
+      const transaction = nftMarket.buyNFT(tokenId, { value: 22 });
+      await expect(transaction).to.be.revertedWith('NFT price incorrect');
+    });
+
+    it('should transfer ownership to the buyer and send the price to the seller', async () => {
+      const price = 123;
+      const sellerProfit = Math.floor((price * 95) / 100);
+      const fee = price - sellerProfit;
+
+      const tokenId = await createAndListNFT(price);
+
+      await new Promise((r) => setTimeout(r, 100));
+      const oldSellerBalance = await signers[0].getBalance();
+
+      const initialContractBalance = await nftMarket.provider.getBalance(
+        nftMarket.address
+      );
+
+      const transaction = await nftMarket
+        .connect(signers[1])
+        .buyNFT(tokenId, { value: price });
+      const receipt = await transaction.wait();
+
+      await new Promise((r) => setTimeout(r, 100));
+      const newSellerBalance = await signers[0].getBalance();
+
+      const diff = newSellerBalance.sub(oldSellerBalance);
+      expect(diff).to.equal(sellerProfit);
+      expect(diff).to.equal(sellerProfit);
+
+      const newContractBalance = await nftMarket.provider.getBalance(
+        nftMarket.address
+      );
+
+      const contractBalanceDiff = newContractBalance.sub(
+        initialContractBalance
+      );
+
+      expect(contractBalanceDiff).to.equal(fee);
+
+      const ownerAddress = await nftMarket.ownerOf(tokenId);
+      expect(ownerAddress).to.equal(signers[1].address);
+
+      const args = receipt.events[1].args;
+
+      expect(args.tokenId).to.equal(tokenId);
+      expect(args.to).to.equal(signers[1].address);
+      expect(args.tokenURI).to.equal('');
+      expect(args.price).to.equal(0);
+    });
   });
 });
